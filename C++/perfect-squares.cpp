f2134174bd67843c22e86e3557e924f17929d877
Howard <hdqh88@gmail.com>
2015-11-21 12:59:16 -0500

    Update perfect-squares.cpp

--- C++/perfect-squares.cpp
+++ C++/perfect-squares.cpp
@@ -4,15 +4,24 @@
 class Solution {
 public:
     int numSquares(int n) {
-        static vector<int> num{0};
-        while (num.size() <= n) {
-            int squares = numeric_limits<int>::max();
-            for (int i = 1; i * i <= num.size(); ++i) {
-                squares = min(squares, num[num.size() - i * i] + 1);
-            }
-            num.emplace_back(squares);
+        if (n <= 0)
+            return 0;
+        // cntPerfectSquares[i] = the least number of perfect square numbers 
+        // which sum to i. Since cntPerfectSquares is a static vector, if 
+        // cntPerfectSquares.size() > n, we have already calculated the result 
+        // during previous function calls and we can just return the result now.
+        static vector<int> cntPerfectSquares({0});
+
+        // While cntPerfectSquares.size() <= n, we need to incrementally 
+        // calculate the next result until we get the result for n.
+        while (cntPerfectSquares.size() <= n) {
+            int m = cntPerfectSquares.size();
+            int cntSquares = INT_MAX;
+            for (int i = 1; i*i <= m; i++)
+                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            cntPerfectSquares.push_back(cntSquares);
         }
-        return num[n];
+        return cntPerfectSquares[n];
     }
 };
 
