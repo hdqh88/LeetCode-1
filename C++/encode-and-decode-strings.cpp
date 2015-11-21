15223005d8df4a69987f0703c9d803a372c41bad
Howard <hdqh88@gmail.com>
2015-11-21 15:16:52 -0500

    Update encode-and-decode-strings.cpp

--- C++/encode-and-decode-strings.cpp
+++ C++/encode-and-decode-strings.cpp
@@ -1,7 +1,37 @@
 // Time:  O(n)
 // Space: O(1)
 
+
 class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string s;
+        for(string str : strs){
+            s += to_string(str.size()) + "@" + str;
+        }
+        return s;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> result;
+        int head = 0;
+        while(head < s.size()){
+            int at_pos = s.find_first_of('@', head);
+            int len = stoi(s.substr(head, at_pos - head));
+            result.push_back(s.substr(at_pos+1, len));
+            head = at_pos + len + 1;
+        }
+        return result;
+    }
+};
+
+
+
+
+class Codec2 {
 public:
 
     // Encodes a list of strings to a single string.
