4a2f84f1d80030eb0d40251df4f0fdd32d0f20d9
Howard <hdqh88@gmail.com>
2015-11-21 15:53:51 -0500

    Update closest-binary-search-tree-value.cpp

--- C++/closest-binary-search-tree-value.cpp
+++ C++/closest-binary-search-tree-value.cpp
@@ -10,7 +10,23 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        int val1 = root->val;
+        TreeNode* p = target < val1 ? root->left : root->right;
+        if(p == NULL)
+            return val1;
+        int val2 = closestValue(p, target);
+        return abs(val1 - target) < abs(val2 - target) ? val1 : val2;
+    }
+};
+
+
+
+ 
+class Solution2 {
 public:
     int closestValue(TreeNode* root, double target) {
         double gap = numeric_limits<double>::max();
