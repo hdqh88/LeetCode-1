390df6cf94b83dfb2e1d9ea7a4581315488f8a70
Howard <hdqh88@gmail.com>
2015-11-24 15:41:08 -0500

    Update flatten-2d-vector.cpp

--- C++/flatten-2d-vector.cpp
+++ C++/flatten-2d-vector.cpp
@@ -1,7 +1,31 @@
 // Time:  O(1)
 // Space: O(1)
 
+
 class Vector2D {
+    vector<vector<int>>::iterator i, iEnd;
+    int j = 0;
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        i = vec2d.begin();
+        iEnd = vec2d.end();
+    }
+
+    int next() {
+        hasNext();
+        return (*i)[j++];
+    }
+
+    bool hasNext() {
+        while (i != iEnd && j == (*i).size())
+            i++, j = 0;
+        return i != iEnd;
+    }
+};
+
+
+
+class Vector2D_2 {
 public:
     Vector2D(vector<vector<int>>& vec2d) : vec(vec2d) {
         x = vec.begin();
