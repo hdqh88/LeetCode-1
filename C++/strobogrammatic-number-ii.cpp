89f9a6c6e536e3ae08543c376029719836da659f
Howard <hdqh88@gmail.com>
2015-11-25 14:36:42 -0500

    Update strobogrammatic-number-ii.cpp

--- C++/strobogrammatic-number-ii.cpp
+++ C++/strobogrammatic-number-ii.cpp
@@ -1,7 +1,31 @@
 // Time:  O(n^2 * 5^(n/2))
 // Space: O(n)
 
+
 class Solution {
+    unordered_map<string, string> dic{{"0", "0"}, {"1", "1"}, {"8", "8"}, {"6", "9"}, {"9", "6"}};
+public:
+    vector<string> findStrobogrammatic(int n) {
+        return recursion(n, n);
+    }
+    
+    vector<string> recursion(int n, int k){
+        if(k == 0)
+            return {""};
+        if(k == 1)
+            return {"0", "1", "8"};
+        vector<string> rec = recursion(n, k - 2);
+        vector<string> result;
+        for(string& str : rec)
+            for(map<string, string>::value_type& ele : dic)
+                if(n != k || ele.first != "0")
+                    result.push_back(ele.first + str + ele.second);
+        return result;
+    }
+};
+
+
+class Solution2 {
 public:
     vector<string> findStrobogrammatic(int n) {
         return findStrobogrammaticRecu(n, n);
