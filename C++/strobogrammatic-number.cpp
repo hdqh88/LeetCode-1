bfd2ddde69814f541db8487b392a25383fd99d53
Howard <hdqh88@gmail.com>
2015-11-25 10:51:20 -0500

    Update strobogrammatic-number.cpp

--- C++/strobogrammatic-number.cpp
+++ C++/strobogrammatic-number.cpp
@@ -1,7 +1,25 @@
 // Time:  O(n)
 // Space: O(1)
 
+
 class Solution {
+public:
+    bool isStrobogrammatic(string num) {
+        unordered_map<char, char> dic{{'0', '0'}, {'1', '1'}, {'6', '9'}, {'9', '6'}, {'8', '8'}};
+        if(num.empty())
+            return false;
+        int i = 0;
+        while(i <= num.size() / 2){
+            if(dic.find(num[i]) == dic.end() || dic[num[i]] != num[num.size()-1-i])
+                return false;
+            i++;
+        }
+        return true;
+    }
+};
+
+
+class Solution2 {
 public:
     bool isStrobogrammatic(string num) {
         const int n = num.size();
