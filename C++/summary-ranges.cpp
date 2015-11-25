ec4bdb3a0a6bf282a1ae4dfff37ccb6bf96f6232
Howard <hdqh88@gmail.com>
2015-11-25 17:34:14 -0500

    Update summary-ranges.cpp

--- C++/summary-ranges.cpp
+++ C++/summary-ranges.cpp
@@ -1,6 +1,34 @@
 // Time:  O(n)
 // Space: O(1)
 
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<string> result;
+        if(n == 0)
+            return result;
+        int start = 0, end = 0;
+        string path;
+        for(int i = 0; i < n; i++){
+            if(start == end)
+                path = to_string(nums[i]);
+            if(i < n - 1 && nums[i+1] == nums[i] + 1)
+                end++;
+            else{ 
+                if(end > start)
+                    path.append("->" + to_string(nums[i]));
+                result.push_back(path);
+                start = end = min(i + 1, n - 1);
+            }
+        }
+        return result;
+    }
+};
+
+
+
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
