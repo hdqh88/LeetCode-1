c4d5059f65a86a3d8655aefee1a4b240970ffbb2
Howard <hdqh88@gmail.com>
2015-11-28 15:42:17 -0500

    Update walls-and-gates.cpp

--- C++/walls-and-gates.cpp
+++ C++/walls-and-gates.cpp
@@ -1,3 +1,36 @@
+// BFS
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        int m = rooms.size();
+        if(m == 0) return;
+        int n = rooms[0].size();
+        queue<pair<int, int> > q;
+        vector<pair<int, int> > dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                if(rooms[i][j] == 0)
+                    q.push(make_pair(i,j));
+        
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            for(pair<int, int>& dir: dirs){
+                int x = i + dir.first;
+                int y = j + dir.second;
+                if(x >= 0 && x < m && y >= 0 && y < n && rooms[x][y] > 0)
+                    if(rooms[x][y] > rooms[i][j] + 1){
+                        rooms[x][y] = rooms[i][j] + 1;
+                        q.push(make_pair(x,y));
+                    }
+            }
+        }
+    }
+};
+
+
+
 // Time:  O(m * n)
 // Space: O(g)
 
