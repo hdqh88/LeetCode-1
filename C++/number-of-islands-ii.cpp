580aea41ca0c14ae782ac5c5d8309601b64e2da4
Howard <hdqh88@gmail.com>
2015-11-30 11:54:31 -0500

    Update number-of-islands-ii.cpp

--- C++/number-of-islands-ii.cpp
+++ C++/number-of-islands-ii.cpp
@@ -7,16 +7,16 @@ public:
     vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
         vector<int> numbers;
         int number = 0;
-        const vector<pair<int, int>> directions{{0, -1}, {0, 1},
+        vector<pair<int, int>> directions{{0, -1}, {0, 1},
                                                 {-1, 0}, {1, 0}};
         unordered_map<int, int> set;
-        for (const auto& position : positions) {
-            const auto& node = make_pair(position.first, position.second);
+        for (auto& position : positions) {
+            auto& node = make_pair(position.first, position.second);
             set[node_id(node, n)] = node_id(node, n);
             ++number;
 
-            for (const auto& d : directions) {
-                const auto& neighbor = make_pair(position.first + d.first,
+            for (auto& d : directions) {
+                auto& neighbor = make_pair(position.first + d.first,
                                                  position.second + d.second);
                 if (neighbor.first >= 0 && neighbor.first < m &&
                     neighbor.second >= 0 && neighbor.second < n &&
