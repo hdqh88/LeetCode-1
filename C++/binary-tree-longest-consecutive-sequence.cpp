e199babb1c0a6797c5af625df16302b965854e57
Howard <hdqh88@gmail.com>
2015-11-30 11:17:29 -0500

    Update binary-tree-longest-consecutive-sequence.cpp

--- C++/binary-tree-longest-consecutive-sequence.cpp
+++ C++/binary-tree-longest-consecutive-sequence.cpp
@@ -10,6 +10,36 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+
+
+class Solution {
+public:
+    int longestConsecutive(TreeNode* root) {
+        int max_length = 0;
+        dfs(root, max_length);
+        return max_length;
+    }
+    
+    int dfs(TreeNode* root, int& max_len){
+        if(!root)
+            return 0;
+        int left_len = dfs(root->left, max_len);
+        int right_len = dfs(root->right, max_len);
+        int cur_len = 1;
+        if(root->left && root->left->val == root->val + 1)
+            cur_len = left_len + 1;
+        if(root->right && root->right->val == root->val + 1)
+            cur_len = max(cur_len, right_len + 1);
+        max_len = max(max_len, max(cur_len, max(left_len, right_len)));
+        return cur_len;
+    }
+};
+
+
+
+
+
 class Solution {
 public:
     int longestConsecutive(TreeNode* root) {
